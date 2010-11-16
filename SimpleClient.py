cff02096f8fdb0c3db2ab2ae4faf1bbf5b5a1984
Fede <gnufede@gmail.com>
2010-11-16 17:32:09 +0100

    Added threading in client

--- SimpleClient.py
+++ SimpleClient.py
@@ -3,13 +3,10 @@ import socket
 import threading
 import time
 
-def client(ip, port, message):
+def client(ip, port, num):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((ip, port))
-    talk2(sock)
-   # sock.send(message)
-   # response = sock.recv(1024)
-   # print "Received: %s" % response
+    talk(sock,num)
     sock.close()
 
 def ask(sock,message):
@@ -19,35 +16,19 @@ def ask(sock,message):
     print response
     return response
 
-def talk(sock):
-    login = "xxx1"
+def talk(sock,num):
+    login = "xxx%d" % num
     pos = "0.0,0.0,1"
-    destination = "0.0,0.1,1"
-    response = ask(sock,"login "+login)
-    if (response == ("Hello " + login)):
-        response = ask(sock,"pos "+pos)
-        if (response == ("POS " + pos)):
-            response = ask(sock,"dest "+destination)
-            if (response == ("DEST " + destination)):
-                response = ask(sock, "instructions please")
-                print "Received: %s" % response
-
-def talk2(sock):
-    login = "xxx1"
-    pos = "0.0,0.0,1"
-    destination = "0.0,0.1,1"
+    destination = "0.0,0.%d,1" %num
     instructions = "please"
     response = ask(sock,("login=%s pos=%s destination=%s instructions=%s" % (login,pos,destination,instructions)))
-    print "Received: %s" % response
+    print "%s Received: %s" % (login,response)
 
 
 if __name__ == "__main__":
     # Port 0 means to select an arbitrary unused port
     ip, port = "localhost", 9000
-
-    client(ip, port, "Hello World 1")
-    client(ip, port, "Hello World 2")
-    client(ip, port, "Hello World 3")
-
-
+    for  i in range(0, 10):
+        t = threading.Thread(target=client, args=(ip, port,  i, ))  
+        t.start() 
 
--- SuperServer.py
+++ SuperServer.py
@@ -7,28 +7,16 @@ import time
 
 class ThreadedTCPRequestHandler(SocketServer.BaseRequestHandler):
 
-    def talk(self):
-        while (True):
-            data = self.request.recv(1024)
-            answer = data.split()
-            if answer:
-                 if answer[0] == "bye":
-                     self.request.send("Bye "+answer[1])
-                     break
-                 elif answer[0] == "login":
-                     login = answer[1]
-                     self.request.send("Hello "+answer[1])
-                 elif answer[0] == "pos":
-                     pos = answer[1]
-                     self.request.send("POS "+answer[1])
-                 elif answer[0] == "dest":
-                     dest = answer[1]
-                     self.request.send("DEST "+answer[1])
-                 elif data == "instructions please":
-                     self.request.send("Just Go Ahead")
-                 else: self.request.send("Sorry?")
+    def process(self, pict):
+        instr = ""
+        if pict["destination"] == pict["pos"]:
+            instr = "You reached your destination"
+        else:
+            instr = "Just go ahead and leave me alone, please"
+        return instr
+        
 
-    def talk2(self):
+    def talk(self):
         pict = {}
         data = self.request.recv(1024)
         answer = data.split()
@@ -38,27 +26,15 @@ class ThreadedTCPRequestHandler(SocketServer.BaseRequestHandler):
                 pict[v[0]] = v[1]
         print pict
         #XXX
-        time.sleep(5)
-        self.request.send("Just go ahead and leave me alone, please")
+        time.sleep(2)
+        self.request.send(self.process(pict))
 
 
     def handle(self):
-        self.talk2()
-        #data = self.request.recv(1024)
-        #cur_thread = threading.currentThread()
-        #response = "%s: %s" % (cur_thread.getName(), data)
-        #self.request.send(response)
+        self.talk()
 
 class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
     pass
-
-def client(ip, port, message):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect((ip, port))
-    sock.send(message)
-    response = sock.recv(1024)
-    print "Received: %s" % response
-    sock.close()
 
 if __name__ == "__main__":
     # Port 0 means to select an arbitrary unused port
@@ -74,9 +50,5 @@ if __name__ == "__main__":
     #server_thread.setDaemon(True)
     server_thread.start()
     print "Server loop running in thread:", server_thread.getName()
-
-#    client(ip, port, "Hello World 1")
-#    client(ip, port, "Hello World 2")
-#    client(ip, port, "Hello World 3")
 
 #    server.shutdown()
