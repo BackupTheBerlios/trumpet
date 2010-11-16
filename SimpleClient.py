3dbc63cf9ad65084858271faa64c798084cf6877
Fede <gnufede@gmail.com>
2010-11-16 16:50:58 +0100

    Server and client working

--- SimpleClient.py
+++ SimpleClient.py
@@ -1,14 +1,44 @@
 #!/usr/bin/python
 import socket
 import threading
+import time
 
 def client(ip, port, message):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((ip, port))
+    talk2(sock)
+   # sock.send(message)
+   # response = sock.recv(1024)
+   # print "Received: %s" % response
+    sock.close()
+
+def ask(sock,message):
+    print message
     sock.send(message)
     response = sock.recv(1024)
+    print response
+    return response
+
+def talk(sock):
+    login = "xxx1"
+    pos = "0.0,0.0,1"
+    destination = "0.0,0.1,1"
+    response = ask(sock,"login "+login)
+    if (response == ("Hello " + login)):
+        response = ask(sock,"pos "+pos)
+        if (response == ("POS " + pos)):
+            response = ask(sock,"dest "+destination)
+            if (response == ("DEST " + destination)):
+                response = ask(sock, "instructions please")
+                print "Received: %s" % response
+
+def talk2(sock):
+    login = "xxx1"
+    pos = "0.0,0.0,1"
+    destination = "0.0,0.1,1"
+    instructions = "please"
+    response = ask(sock,("login=%s pos=%s destination=%s instructions=%s" % (login,pos,destination,instructions)))
     print "Received: %s" % response
-    sock.close()
 
 
 if __name__ == "__main__":
--- SuperServer.py
+++ SuperServer.py
@@ -2,25 +2,63 @@
 import socket
 import threading
 import SocketServer
+import time
+
 
 class ThreadedTCPRequestHandler(SocketServer.BaseRequestHandler):
 
-    def handle(self):
+    def talk(self):
+        while (True):
+            data = self.request.recv(1024)
+            answer = data.split()
+            if answer:
+                 if answer[0] == "bye":
+                     self.request.send("Bye "+answer[1])
+                     break
+                 elif answer[0] == "login":
+                     login = answer[1]
+                     self.request.send("Hello "+answer[1])
+                 elif answer[0] == "pos":
+                     pos = answer[1]
+                     self.request.send("POS "+answer[1])
+                 elif answer[0] == "dest":
+                     dest = answer[1]
+                     self.request.send("DEST "+answer[1])
+                 elif data == "instructions please":
+                     self.request.send("Just Go Ahead")
+                 else: self.request.send("Sorry?")
+
+    def talk2(self):
+        pict = {}
         data = self.request.recv(1024)
-        cur_thread = threading.currentThread()
-        response = "%s: %s" % (cur_thread.getName(), data)
-        self.request.send(response)
+        answer = data.split()
+        for item in answer:
+            v = item.split('=')
+            if not v[0] in pict:
+                pict[v[0]] = v[1]
+        print pict
+        #XXX
+        time.sleep(5)
+        self.request.send("Just go ahead and leave me alone, please")
+
+
+    def handle(self):
+        self.talk2()
+        #data = self.request.recv(1024)
+        #cur_thread = threading.currentThread()
+        #response = "%s: %s" % (cur_thread.getName(), data)
+        #self.request.send(response)
 
 class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
     pass
 
-#def client(ip, port, message):
-#    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#    sock.connect((ip, port))
-#    sock.send(message)
-#    response = sock.recv(1024)
-#    print "Received: %s" % response
-#    sock.close()
+def client(ip, port, message):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((ip, port))
+    sock.send(message)
+    response = sock.recv(1024)
+    print "Received: %s" % response
+    sock.close()
 
 if __name__ == "__main__":
     # Port 0 means to select an arbitrary unused port
