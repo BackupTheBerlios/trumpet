39a0288c8b71b772e190e4f95e5b1d3b9d126b0e
Fede <gnufede@gmail.com>
2010-11-17 20:12:39 +0100

    Superserver asks GiveServer for instructions

--- GiveServer.py
+++ GiveServer.py
@@ -2,32 +2,54 @@
 import socket
 import threading
 import SocketServer
+import time
 
-class GiveTCPRequestHandler(SocketServer.BaseRequestHandler):
+
+class ThreadedTCPRequestHandler(SocketServer.BaseRequestHandler):
+
+    ''' Method that should take care of processing a request'''
+    def process(self, pict):
+        instr = ""
+        if pict["destination"] == pict["pos"]:
+            instr = "You reached your destination"
+        else:
+            instr = "Just go ahead and leave me alone, please"
+        return instr
+        
+    def getDictValues(self,dic,values):
+        string_values = ''
+        for field in values:
+            if field in dic:
+                string_values = string_values+field+"="+dic.get(field)+' '
+        string_values = string_values[0:-1] #Hack to remove last whitespace
+        print string_values
+        return string_values
+    
 
     def talk(self):
+        pict = {}
         data = self.request.recv(1024)
-        if data == "pos 0.0,0.0,1 dest 0.0,0.1,1":
-           response = "Go Ahead"
-           self.request.send(response)
-        
+        answer = data.split()
+        for item in answer:
+            v = item.split('=')
+            if v[0] and v[1] and not v[0] in pict:
+                pict[v[0]] = v[1]
+        print pict #XXX
+        time.sleep(2) #XXX
+        self.request.send(self.process(pict))
+
 
     def handle(self):
         self.talk()
-        #data = self.request.recv(1024)
-        #cur_thread = threading.currentThread()
-        #response = "%s: %s" % (cur_thread.getName(), data)
-        #self.request.send(response)
 
-class GiveTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
+class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
     pass
-
 
 if __name__ == "__main__":
     # Port 0 means to select an arbitrary unused port
     HOST, PORT = "localhost", 9001
 
-    server = GiveTCPServer((HOST, PORT), GiveTCPRequestHandler)
+    server = ThreadedTCPServer((HOST, PORT), ThreadedTCPRequestHandler)
     ip, port = server.server_address
 
     # Start a thread with the server -- that thread will then start one
@@ -36,10 +58,6 @@ if __name__ == "__main__":
     # Exit the server thread when the main thread terminates
     #server_thread.setDaemon(True)
     server_thread.start()
-    print "Give Server loop running in thread:", server_thread.getName()
-
-#    client(ip, port, "Hello World 1")
-#    client(ip, port, "Hello World 2")
-#    client(ip, port, "Hello World 3")
+    print "Server loop running in thread:", server_thread.getName() #XXX
 
 #    server.shutdown()
--- SuperServer.py
+++ SuperServer.py
@@ -7,13 +7,36 @@ import time
 
 class ThreadedTCPRequestHandler(SocketServer.BaseRequestHandler):
 
+    ''' Method that should take care of processing a request'''
     def process(self, pict):
-        instr = ""
-        if pict["destination"] == pict["pos"]:
-            instr = "You reached your destination"
-        else:
-            instr = "Just go ahead and leave me alone, please"
+        # Process (in this case, ask the GIVE server for instructions)
+        instr = self.askGive(pict)
+   #     instr = ""
+   #     if pict["destination"] == pict["pos"]:
+   #         instr = "You reached your destination"
+   #     else:
+   #         instr = "Just go ahead and leave me alone, please"
         return instr
+        
+    def dictValuesToStr(self,dic,values):
+        string_values = ''
+        for field in values:
+            if field in dic:
+                string_values = string_values+field+"="+dic.get(field)+' '
+        string_values = string_values[0:-1] #Hack to remove last whitespace
+        print string_values
+        return string_values
+    
+
+    ''' Method that asks the GIVE server for instructions'''
+    def askGive(self, pict):
+        ip, port = "localhost", 9001
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((ip, port))
+        sock.send(self.dictValuesToStr(pict,['destination','pos']))
+        response = sock.recv(1024)
+        sock.close()
+        return response
         
 
     def talk(self):
@@ -22,11 +45,10 @@ class ThreadedTCPRequestHandler(SocketServer.BaseRequestHandler):
         answer = data.split()
         for item in answer:
             v = item.split('=')
-            if not v[0] in pict:
+            if v[0] and v[1] and not v[0] in pict:
                 pict[v[0]] = v[1]
-        print pict
-        #XXX
-        time.sleep(2)
+        print pict #XXX
+        time.sleep(2) #XXX
         self.request.send(self.process(pict))
 
 
@@ -49,6 +71,6 @@ if __name__ == "__main__":
     # Exit the server thread when the main thread terminates
     #server_thread.setDaemon(True)
     server_thread.start()
-    print "Server loop running in thread:", server_thread.getName()
+    print "Server loop running in thread:", server_thread.getName() #XXX
 
 #    server.shutdown()
